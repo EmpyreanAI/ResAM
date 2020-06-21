b66d257f0b8c3a9e39e5fdd2a5abf69963b45e55
Renato Avellar Nobre <rekanobre@gmail.com>
2020-06-21 18:23:30 -0300

    Updated doc reqs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ html_theme_options = {
     'display_version': True
 }
 
-autodoc_mock_imports = ["b3data", "tensorflow"]
+autodoc_mock_imports = ["b3data", "tensorflow", "spinup"]
 
 def setup(app):
     app.add_css_file('css/modify.css')
