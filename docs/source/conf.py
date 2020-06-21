2f83277b226192c191574badc21d3403cd1f686a
Renato Avellar Nobre <rekanobre@gmail.com>
2020-06-21 18:58:03 -0300

    Fixing thing I dont even know

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/resam.png'
+html_logo = '_static/resam.svg'
 
 html_theme_options = {
     'logo_only': True,
