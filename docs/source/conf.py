d3d2a2a0da6b53344ba5b0a2fa728acca49c5d4b
Renato Avellar Nobre <rekanobre@gmail.com>
2020-06-21 18:48:54 -0300

    Mocking more imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ html_theme_options = {
     'display_version': True
 }
 
-autodoc_mock_imports = ["b3data", "tensorflow", "spinup"]
+autodoc_mock_imports = ["b3data", "tensorflow", "spinup", "plotly", "pandas"]
 
 def setup(app):
     app.add_css_file('css/modify.css')
