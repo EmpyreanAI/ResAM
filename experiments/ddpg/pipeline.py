5291c471c98b56b017530b913c3bcb585f7ae115
Khalil Carsten <khalilcarsten@gmail.com>
2020-07-08 13:53:21 -0300

    pipeline experiment script

---
+++ experiments/ddpg/pipeline.py
@@ -0,0 +1,21 @@
+import yaml
+from subprocess import call
+
+file = open('EXPERIMENTS')
+try:
+    experiments = yaml.full_load(file)
+except:
+    raise
+
+for item, doc in experiments.items():
+    args_list = []
+    args_list.extend(doc['_configs'].values())
+    args_list.append(len(doc['_stocks']))
+    args_list.extend(doc['_stocks'])
+    args_list.extend(doc['_windows'])
+    args_list.append(doc['_start_year'])
+    args_list.append(doc['_end_year'])
+    exec = ["python3", "resam.py"]
+    for i in args_list:
+        exec.append(str(i))
+    call(exec)
